6def641485622d743fe8df5316bf2186e14b7ab7
Bruno Guidolim <bguidolim@gmail.com>
2020-04-07 17:31:18 +0200

    Added Carthage support

---
+++ CancellablePromiseKit/CancellablePromiseKit.h
@@ -0,0 +1,19 @@
+//
+//  CancellablePromiseKit.h
+//  CancellablePromiseKit
+//
+//  Created by Bruno Guidolim on 07.04.20.
+//  Copyright © 2020 COYO GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CancellablePromiseKit.
+FOUNDATION_EXPORT double CancellablePromiseKitVersionNumber;
+
+//! Project version string for CancellablePromiseKit.
+FOUNDATION_EXPORT const unsigned char CancellablePromiseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CancellablePromiseKit/PublicHeader.h>
+
+
