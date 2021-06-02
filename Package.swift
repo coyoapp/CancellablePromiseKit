8d4127befdae0d48c15ab5222db7c98fc808be4c
Vladyslava Kirichenko <velga@users.noreply.github.com>
2021-06-02 12:31:45 +0200

    Update Package.swift
    Increment the ios version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CancellablePromiseKit",
-    platforms: [.iOS(.v8),
+    platforms: [.iOS(.v9),
                 .tvOS(.v9)],
     products: [
         .library(name: "CancellablePromiseKit", targets: ["CancellablePromiseKit"]),
